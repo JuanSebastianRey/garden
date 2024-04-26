26816ec5caf601d6e20d8afa5435a62fc880179f
JuanSebastianRey <sebastianreyes25@outlook.es>
2024-04-26 13:05:12 -0500

    feat: :sparkles: Ensamble de json
    Se ensamblo las consultas 1 y 2 de los json

--- js/report.js
+++ js/report.js
@@ -1,4 +1,54 @@
 import "./components/clock.js";
+import { getAllOfficesCodeAndCity } from "./module/offices.js";
+const queryAboutTable1 = document.querySelector("#queryAboutTable1");
+queryAboutTable1.addEventListener("click", async(e)=>{
+    let [,report__container] = queryAboutTable1.children
+    if(!report__container.innerHTML){
+        let data = await getAllOfficesCodeAndCity();
+        let plantilla = "";
+        console.log(data);
+        data.forEach(val => {
+            plantilla += `
+                <div class="report__card">
+                <div class="card__title">
+                    <div>${val.city}</div>
+                </div>
+                <div class="card__body">
+                    <div class="body__marck">
+                        <p><b>Codigo de la oficina: </b>${val.code}</p>
+                    </div>
+                </div>
+            </div>
+            `;
+        });
+        report__container.innerHTML = plantilla;
+    }
+})
+import { getAllOfficesFromSpainCityAndMovil } from "./module/offices.js";
+const queryAboutTable2 = document.querySelector("#queryAboutTable2");
+queryAboutTable2.addEventListener("click", async(e)=>{
+    let [,report__container] = queryAboutTable2.children
+    if(!report__container.innerHTML){
+        let data = await getAllOfficesFromSpainCityAndMovil();
+        let plantilla = "";
+        console.log(data);
+        data.forEach(val => {
+            plantilla += `
+                <div class="report__card">
+                <div class="card__title">
+                    <div>${val.city}</div>
+                </div>
+                <div class="card__body">
+                    <div class="body__marck">
+                        <p><b>Numero de la oficina: </b>${val.telephone}</p>
+                    </div>
+                </div>
+            </div>
+            `;
+        });
+        report__container.innerHTML = plantilla;
+    }
+})
 import { getClientsEmploy } from "./module/clients.js";
 const queryAboutTable7 = document.querySelector("#queryAboutTable7");
 queryAboutTable7.addEventListener("click", async(e)=>{
